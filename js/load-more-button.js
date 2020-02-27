0ba09aa61d4c40def9d57b1a9387aa5388d20298
Thomas Grylka <50058009+thomasgrylka@users.noreply.github.com>
2020-02-27 17:22:01 -0500

    disable

--- js/load-more-button.js
+++ js/load-more-button.js
@@ -1,14 +1,14 @@
-$(document).ready(function () {
-    size_li = $("#latestEntries li").size();
-    x=3;
-    $('#latestEntries li:lt('+x+')').show();
-    $('#loadMore').click(function () {
-        x= (x+3 <= size_li) ? x+3 : size_li;
-        $('#latestEntries li:lt('+x+')').slideDown(600);
+// $(document).ready(function () {
+//     size_li = $("#latestEntries li").size();
+//     x=3;
+//     $('#latestEntries li:lt('+x+')').show();
+//     $('#loadMore').click(function () {
+//         x= (x+3 <= size_li) ? x+3 : size_li;
+//         $('#latestEntries li:lt('+x+')').slideDown(600);
         
-    });
-    $('#showLess').click(function () {
-        x=(x-3<0) ? 3 : x-3;
-        $('#latestEntries li').not(':lt('+x+')').slideUp(600);
-    });
-});
+//     });
+//     $('#showLess').click(function () {
+//         x=(x-3<0) ? 3 : x-3;
+//         $('#latestEntries li').not(':lt('+x+')').slideUp(600);
+//     });
+// });
